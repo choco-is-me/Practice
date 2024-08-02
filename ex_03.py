e55a9040c48c52c5a0dd0ad2d1f02edf298e5805
thebao2307 <thebao1426@gmail.com>
2024-08-03 02:21:53 +0700

    remove python files

--- ex_03.py
+++
@@ -1,9 +0,0 @@
-import itertools
-
-def cross(A, B):
-    return {a + b for a in A for b in B}
-
-urn = cross('M', '1234') | cross('P', '123') | cross('C', '12') | cross('L', '1')
-
-U1 = list(itertools.permutations(urn, 10))
-print(len(U1))
